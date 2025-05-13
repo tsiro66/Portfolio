245b292bd3358b17de0dc17048bc95198e710dff
tsiro66 <theogr99@gmail.com>
2025-05-13 17:16:09 +0300

    Progress bar is green if progress is 100%

--- src/components/Projects/Card.tsx
+++ src/components/Projects/Card.tsx
@@ -16,24 +16,26 @@ const DemoCard: React.FC<DemoCardProps> = ({
   return (
     <>
       <div
-        className="bg-gray-100 dark:bg-gray-800 rounded-2xl p-6 flex flex-col border border-gray-200 dark:border-gray-700 
-      transition-all transform duration-300 hover:scale-105 cursor-pointer shadow-md hover:shadow-lg "
+        className='bg-gray-100 dark:bg-gray-800 rounded-2xl p-6 flex flex-col border border-gray-200 dark:border-gray-700 
+      transition-all transform duration-300 hover:scale-105 cursor-pointer shadow-md hover:shadow-lg '
         onClick={() => {
           if (link) {
             window.open(link, "_blank");
           }
         }}
       >
-        <h2 className="flex items-center text-xl font-bold gap-2 mb-2">
+        <h2 className='flex items-center text-xl font-bold gap-2 mb-2'>
           {title} {techstack}
         </h2>
-        <p className="text-gray-400 dark:text-gray-300 mb-4">{description}</p>
+        <p className='text-gray-400 dark:text-gray-300 mb-4'>{description}</p>
         {progress && (
           <>
-            <p className="text-gray-400 dark:text-gray-300">{progress}</p>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+            <p className='text-gray-400 dark:text-gray-300'>{progress}</p>
+            <div className='w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
               <div
-                className="bg-blue-600 h-2.5 rounded-full"
+                className={`h-2.5 rounded-full ${
+                  progress === "100%" ? "bg-green-500" : "bg-blue-600"
+                }`}
                 style={{ width: progress }}
               ></div>
             </div>
