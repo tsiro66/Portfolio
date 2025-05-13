87cdfc2289d79c8d37c45313547e4ef8c5ee0b7c
tsiro66 <theogr99@gmail.com>
2025-05-13 17:07:51 +0300

    Resume AI 45% -> 65%

--- src/components/Projects/ProjectCards.tsx
+++ src/components/Projects/ProjectCards.tsx
@@ -7,50 +7,50 @@ import { RiNextjsFill } from "react-icons/ri";
 
 const ProjectCards = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-4">
+    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-4'>
       <Card
-        title="Healthcare App"
-        description="Fullstack web application for managing healthcare data."
+        title='Healthcare App'
+        description='Fullstack web application for managing healthcare data.'
         techstack={
-          <div className="flex space-x-2">
+          <div className='flex space-x-2'>
             <RiReactjsLine />
             <SiMui />
             <BiLogoPostgresql />
             <SiSpring />
           </div>
         }
-        link="https://github.com/stars/tsiro66/lists/healthcare-app"
-        progress="90%"
+        link='https://github.com/stars/tsiro66/lists/healthcare-app'
+        progress='90%'
       />
       <Card
-        title="CV AI Improvement Tool"
-        description="Fullstack web application for improving your CV with AI."
+        title='CV AI Improvement Tool'
+        description='Fullstack web application for improving your CV with AI.'
         techstack={
-          <div className="flex space-x-2">
+          <div className='flex space-x-2'>
             <RiNextjsFill />
             <RiTailwindCssFill />
             <SiPrisma />
             <SiMongodb />
           </div>
         }
-        link="https://github.com/tsiro66/Resume-ai"
-        progress="45%"
+        link='https://github.com/tsiro66/Resume-ai'
+        progress='65%'
       />
       <Card
-        title="Email Reply Assistant"
-        description="Generate professional replies to emails with AI."
-        progress="0%"
+        title='Email Reply Assistant'
+        description='Generate professional replies to emails with AI.'
+        progress='0%'
       />
 
       <Card
-        title="More to come..."
-        description="Stay tuned for more projects!"
+        title='More to come...'
+        description='Stay tuned for more projects!'
         techstack={
-          <div className="flex space-x-2">
+          <div className='flex space-x-2'>
             <RiReactjsLine />
           </div>
         }
-        link=""
+        link=''
       />
     </div>
   );
