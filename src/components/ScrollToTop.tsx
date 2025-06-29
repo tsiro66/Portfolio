21845685ff9ce208a1c7133b251e0a9e2824ffd1
tsiro66 <116112100+tsiro66@users.noreply.github.com>
2025-06-30 01:50:48 +0300

    Netlify.toml

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -5,15 +5,7 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // Instant scroll for better UX on route change
     window.scrollTo(0, 0);
-
-    // Alternative: Smooth scroll (might feel slower)
-    // window.scrollTo({
-    //   top: 0,
-    //   left: 0,
-    //   behavior: "smooth"
-    // });
   }, [pathname]);
 
   return null;
